138273ab23a10a78d723f41dbd29495344c7e47c
Batug Kocak <batugkocak@gmail.com>
2024-09-02 13:30:42 +0300

    # About API has been completed.

---
+++ src/PortGen.Application.Contracts/Abstract/IAboutApplicationService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PortGen.DTOs;
+using Volo.Abp.Application.Services;
+
+namespace PortGen.Abstract;
+
+public interface IAboutApplicationService : IApplicationService
+{
+    public void CreateAbout(AboutDto skillDto);
+    public Task<IEnumerable<AboutDto>> GetAboutAsync();
+    
+    public Task<AboutDto> UpdateAboutAsync(AboutDto aboutDto);
+    
+    public Task DeleteAboutAsync(Guid id);
+}
